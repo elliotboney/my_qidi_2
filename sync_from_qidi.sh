ec7fe41d50c5d39c4c7652dba44969e5d426eb6a
Elliot Boney <elliotboney@gmail.com>
2026-03-19 14:34:23 -0500

    first commit

---
+++ sync_from_qidi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rsync -avz mks@qidi:~/printer_data/ printer_data/ --exclude backup --exclude gcodes/ --exclude logs/ --exclude "*.log" --exclude timelapse/ --exclude ".cache" --exclude ".temp" --exclude ".git" --exclude "comms/" --exclude "database/"
+# rsync -avz mks@qidi:~/fluidd-config/ fluidd-config/ --exclude ".git"
+# rsync -avz mks@qidi:~/Klipper-Adaptive-Meshing-Purging/ Klipper-Adaptive-Meshing-Purging/  --exclude ".git"
+# rsync -avz mks@qidi:~/moonraker-timelapse/ moonraker-timelapse/ --exclude ".git"
